6673dde55f9babff8375334ea41969ba86456290
Rocha <felipe.correa.rocha14@gmail.com>
2022-10-23 20:22:50 -0300

    Ciação de tela de finalizar vendas

---
+++ src/main/java/com/example/vendadecomputadoresjdbc/ComprasController.java
@@ -0,0 +1,9 @@
+package com.example.vendadecomputadoresjdbc;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+
+public class ComprasController {
+        @FXML
+        private RadioButton formaPagamento;
+
+}
